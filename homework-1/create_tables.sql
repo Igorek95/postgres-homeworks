ff39bbf0d258d7d934b274d081504d434a96f955
Garik95 <dzerbuni@mail.ru>
2023-09-21 23:41:14 +0300

    homework1 day1

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,26 @@
 -- SQL-команды для создания таблиц
+CREATE TABLE employees
+(
+	employee_id int PRIMARY KEY,
+	first_name varchar(100) NOT NULL,
+	last_name varchar(100) NOT NULL,
+	title varchar(100) NOT NULL,
+	birth_date varchar(10) NOT NULL,
+	notes varchar(500) NOT NULL
+);
+
+CREATE TABLE customers
+(
+	customer_id text PRIMARY KEY,
+	company_name varchar(100) NOT NULL,
+	contact_name varchar(100) NOT NULL
+);
+
+CREATE TABLE orders
+(
+	order_id int PRIMARY KEY,
+	customer_id text,
+	employee_id int,
+	order_date varchar(10) NOT NULL,
+	ship_city varchar(50) NOT NULL
+)
--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,27 @@
-"""Скрипт для заполнения данными таблиц в БД Postgres."""
+import psycopg2
+import csv
+
+# Считываем пароль для подключения к базе данных из файла
+password_db = open("/home/garik/PycharmProjects/postgres-homeworks2/password_db.txt").read().strip()
+
+# Считываем данные из CSV-файла
+with open('/home/garik/PycharmProjects/postgres-homeworks2/homework-1/north_data/customers_data.csv', newline='') as csvfile:
+    data_reader = csv.reader(csvfile)
+    next(data_reader)  # Пропускаем заголовок, если он есть
+
+    # Подключаемся к базе данных
+    with psycopg2.connect(host='localhost', database='north', user='postgres', password='1234') as conn:
+        with conn.cursor() as cur:
+            for row in data_reader:
+                # Преобразуем данные из CSV в кортеж и вставляем их в базу данных
+                cur.execute('INSERT INTO customers VALUES (%s, %s, %s)', tuple(row))
+
+            # Выполняем запрос на выборку данных
+            cur.execute('SELECT * FROM customers')
+
+            rows = cur.fetchall()
+            for row in rows:
+                print(row)
+
+# Закрываем соединение с базой данных
+conn.close()
