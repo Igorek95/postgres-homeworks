97b2b9d79e817716ee7a3341f8c969b355024421
Garik95 <dzerbuni@mail.ru>
2023-09-22 22:17:19 +0300

    homework1 day2

--- homework-1/main.py
+++ homework-1/main.py
@@ -4,24 +4,34 @@ import csv
 # Считываем пароль для подключения к базе данных из файла
 password_db = open("/home/garik/PycharmProjects/postgres-homeworks2/password_db.txt").read().strip()
 
-# Считываем данные из CSV-файла
-with open('/home/garik/PycharmProjects/postgres-homeworks2/homework-1/north_data/customers_data.csv', newline='') as csvfile:
-    data_reader = csv.reader(csvfile)
-    next(data_reader)  # Пропускаем заголовок, если он есть
 
-    # Подключаемся к базе данных
-    with psycopg2.connect(host='localhost', database='north', user='postgres', password='1234') as conn:
-        with conn.cursor() as cur:
+def add_db(path_file, name_table):
+    try:
+        with open(path_file,
+                  newline='') as csvfile:
+            data_reader = csv.reader(csvfile)
+            header = next(data_reader)  # Получаем заголовок
+            data_list = []
             for row in data_reader:
-                # Преобразуем данные из CSV в кортеж и вставляем их в базу данных
-                cur.execute('INSERT INTO customers VALUES (%s, %s, %s)', tuple(row))
+                data_list.append(tuple(row))
 
-            # Выполняем запрос на выборку данных
-            cur.execute('SELECT * FROM customers')
+            # Подключаемся к базе данных
+            with psycopg2.connect(host='localhost', database='north', user='postgres', password='1234') as conn:
+                with conn.cursor() as cur:
+                    # Создаем строку SQL для вставки данных
+                    columns = ', '.join(header)
+                    placeholders = ', '.join(['%s'] * len(header))
+                    sql = f'INSERT INTO {name_table} ({columns}) VALUES ({placeholders})'
 
-            rows = cur.fetchall()
-            for row in rows:
-                print(row)
+                    # Вставляем данные в базу данных
+                    cur.executemany(sql, data_list)
 
-# Закрываем соединение с базой данных
-conn.close()
+    finally:
+        # Закрываем соединение с базой данных
+        conn.close()
+
+
+if __name__ == '__main__':
+    add_db('north_data/customers_data.csv', 'customers')
+    add_db('north_data/employees_data.csv', 'employees')
+    add_db('north_data/orders_data.csv', 'orders')
